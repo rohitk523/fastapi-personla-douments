35b6f2baeb5e3dac4957232ac70383716e2dbff0
Rohit Kale <rohitkale523@gmail.com>
2023-10-05 23:14:39 +0530

    Started application with random data and some exceptions next step is to add file as an input

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
+DocumentData = {"Rohit": {"Aadhar": 123, "Pan": 456, "Phone":7743,},
+                "Renuka": {"Aadhar": 12, "Pan": 45, "Phone":774,},
+                "Sunita": {"Aadhar": 1, "Pan": 4, "Phone":77,}}
+
 @app.get('/')
-async def doc():
-    return {"message": "this is docs"}
+async def doc(name: str):
+    for i in DocumentData.keys():
+        if i == name:
+            return DocumentData[name]
+    else:
+        raise(HTTPException(status_code=404, detail="USER NOT FOUND"))
+        
